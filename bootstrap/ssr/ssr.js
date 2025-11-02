f129fd8125f7d43571d2849441d4e8afac8d1698
vaseapinkov <wasea.pinkov@gmail.com>
2025-11-02 03:30:34 +0200

    Init project

---
+++ bootstrap/ssr/ssr.js
@@ -0,0 +1,19 @@
+import { createInertiaApp } from "@inertiajs/vue3";
+import createServer from "@inertiajs/vue3/server";
+import { renderToString } from "@vue/server-renderer";
+import { createSSRApp, h } from "vue";
+createServer(
+  (page) => createInertiaApp({
+    page,
+    render: renderToString,
+    resolve: (name) => {
+      const pages = /* @__PURE__ */ Object.assign({});
+      return pages[`./Pages/${name}.vue`];
+    },
+    setup({ App, props, plugin }) {
+      return createSSRApp({
+        render: () => h(App, props)
+      }).use(plugin);
+    }
+  })
+);
